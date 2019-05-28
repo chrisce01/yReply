3df88c4ff3dac53d9473122023452f35cfdc7249
Akshat Jagga <akshatjagga@Akshats-MacBook-Pro.local>
2019-05-28 15:48:21 +0530

    First Commit

---
+++ Yreply/MainScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainScreenViewController.swift
+//  Yreply
+//
+//  Created by Akshat Jagga on 27/05/19.
+//  Copyright © 2019 Akshat Jagga. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
